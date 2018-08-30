dfd0a5bcc14993dd7f675d9dc4b05f378a25cf0e
noffle <sww@eight45.net>
2018-08-30 12:33:19 -0700

    wip: failing sync test

--- test/replicate.js
+++ test/replicate.js
@@ -1,6 +1,8 @@
 var test = require('tape')
 var path = require('path')
 var tmp = require('tmp')
+var fs = require('fs')
+var Osm = require('osm-p2p-mem')
 var Syncfile = require('..')
 
 test('try to replicate before ready', function (t) {
@@ -23,3 +25,53 @@ test('try to replicate before ready', function (t) {
     })
   })
 })
+
+test('replicate osm-p2p to syncfile', function (t) {
+  tmp.dir(function (err, dir, cleanup) {
+    t.error(err)
+
+    var osm = Osm()
+    var syncfile
+
+    osm.create({ type: 'node', lat: 1, lon: 1, tags: { foo: 'bar' } }, function (err) {
+      t.error(err, 'node creation ok')
+      setup()
+    })
+
+    function setup () {
+      var filepath = path.join(dir, 'sync.tar')
+      syncfile = Syncfile(filepath, dir)
+      syncfile.ready(sync)
+    }
+
+    function sync (err) {
+      t.error(err, 'syncfile setup ok')
+      var d = syncfile.createDatabaseReplicationStream()
+      d.once('error', function (err) { t.error(err) })
+
+      replicate(osm.log.replicate(), d, check)
+    }
+
+    function check (err) {
+      t.error(err, 'replication ok')
+      t.equals(fs.readdirSync(syncfile._tmpdir).length, 1, 'tmpdir has db')
+      t.end()
+    }
+  })
+})
+
+function replicate (stream1, stream2, cb) {
+  stream1.on('end', done)
+  stream1.on('error', done)
+  stream2.on('end', done)
+  stream2.on('error', done)
+
+  stream1.pipe(stream2).pipe(stream1)
+
+  var pending = 2
+  var error
+  function done (err) {
+    error = err || error
+    if (!--pending) cb(err)
+  }
+}
