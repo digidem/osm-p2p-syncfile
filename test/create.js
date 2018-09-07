d9c5d875bade9a4100304cb373c1202e8f682e31
noffle <sww@eight45.net>
2018-09-07 16:08:51 -0700

    test: freshly failing test

--- test/create.js
+++ test/create.js
@@ -2,6 +2,7 @@ var test = require('tape')
 var path = require('path')
 var fs = require('fs')
 var tmp = require('tmp')
+var tar = require('tar-stream')
 var Syncfile = require('..')
 
 test('bad creation', function (t) {
@@ -58,6 +59,51 @@ test('can initialize with an existing syncfile', function (t) {
       var syncfile = Syncfile(filepath, dir)
       syncfile.ready(function () {
         syncfile.close(cb)
+      })
+    }
+  })
+})
+
+test('updates to inner osm-p2p-db.tar entry results in old entry being cleared', function (t) {
+  tmp.dir(function (err, dir, cleanup) {
+    t.error(err)
+
+    var filepath = path.join(dir, 'sync.tar')
+
+    setupAndClose(function (err) {
+      t.error(err)
+      t.ok(fs.existsSync(filepath))
+      t.equal(fs.readdirSync(dir).length, 1)
+      t.notEqual(fs.readdirSync(dir).indexOf('sync.tar'), -1)
+
+      setupAndClose(function (err) {
+        t.error(err)
+        t.ok(fs.existsSync(filepath))
+        t.equal(fs.readdirSync(dir).length, 1)
+        t.notEqual(fs.readdirSync(dir).indexOf('sync.tar'), -1)
+
+        var seen = []
+        var ex = tar.extract()
+        fs.createReadStream(filepath).pipe(ex)
+        ex.on('entry', function (header, stream, next) {
+          seen.push(header.name)
+          stream.resume()
+          stream.on('end', next)
+        })
+        ex.on('finish', function () {
+          t.deepEquals(seen.sort(), ['___index.json', 'osm-p2p-db.tar'])
+          t.end()
+        })
+      })
+    })
+
+    function setupAndClose (cb) {
+      var syncfile = Syncfile(filepath, dir)
+      syncfile.ready(function () {
+        syncfile.osm.create({ type: 'node' }, function (err) {
+          t.error(err)
+          syncfile.close(cb)
+        })
       })
     }
   })
