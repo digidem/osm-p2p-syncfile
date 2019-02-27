139d175f47d01750c9984f56d996f0b3b3c7d88e
Karissa McKelvey <krmckelv@gmail.com>
2019-02-26 17:49:43 -0800

    first pass

--- test/replicate.js
+++ test/replicate.js
@@ -1,5 +1,7 @@
 var test = require('tape')
 var path = require('path')
+var fs = require('fs')
+var parallel = require('run-parallel')
 var tmp = require('tmp')
 var OsmMem = require('osm-p2p-mem')
 var blob = require('abstract-blob-store')
@@ -26,6 +28,53 @@ test('try to replicate before ready', function (t) {
 })
 
 test('replicate media + osm-p2p to syncfile', function (t) {
+  tmp.dir(function (err, dir, cleanup) {
+    t.error(err)
+
+    var osm = OsmMem()
+    var media = blob()
+    var syncfile
+
+    osm.create({ type: 'node', lat: 1, lon: 1, tags: { foo: 'bar' } }, function (err, id, theNode) {
+      t.error(err, 'node creation ok')
+      setup()
+    })
+
+    function setup () {
+      var filepath = path.join(dir, 'sync.tar')
+      syncfile = Syncfile(filepath, dir)
+      syncfile.ready(addMedia)
+    }
+
+    function addMedia (err) {
+      t.error(err, 'syncfile setup ok')
+      var tasks = []
+      for (var i = 0; i < 500; i++) {
+        tasks.push((next) => {
+          var writeStream = media.createWriteStream('river.jpg', next)
+          fs.createReadStream('hi-res.jpg').pipe(writeStream)
+        })
+      }
+      parallel(tasks, function (err) {
+        sync()
+      })
+    }
+
+    function sync (err) {
+      t.error(err, 'add media ok')
+      var d = syncfile.osm.log.replicate({live: false})
+      var r = osm.log.replicate({live: false})
+      replicate(r, d, function (err) {
+        t.error(err, 'replicate osm ok')
+        var d = blobReplicate(syncfile.media)
+        var r = blobReplicate(media)
+        replicate(d, r, check)
+      })
+    }
+  })
+})
+
+test('replicate media + osm-p2p to syncfile with large data', function (t) {
   tmp.dir(function (err, dir, cleanup) {
     t.error(err)
 
