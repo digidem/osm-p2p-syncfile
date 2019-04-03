d2999e9ca684b481f4091303ec4916ae0a34733c
noffle <sww@eight45.net>
2019-04-02 17:27:12 -0700

    test: replicate with lots of media'
    originally written by karissa (thanks!)

--- test/replicate.js
+++ test/replicate.js
@@ -9,6 +9,9 @@ var ram = require('random-access-memory')
 var blob = require('abstract-blob-store')
 var blobReplicate = require('blob-store-replication-stream')
 var collect = require('collect-stream')
+var fs = require('fs')
+var parallel = require('run-parallel')
+var fsblob = require('safe-fs-blob-store')
 var Syncfile = require('..')
 
 blob.prototype._list = function (cb) {
@@ -189,6 +192,109 @@ test('replicate osm-p2p + media to new syncfile, close, then reopen & check', fu
         collect(media.createReadStream('river.jpg'), function (err, data) {
           t.error(err, 'read media ok')
           t.equals(data.toString(), '<IMG DATA>')
+          t.end()
+        })
+      })
+    }
+  })
+})
+
+test.only('replicate media + osm-p2p to syncfile with big data', function (t) {
+  tmp.dir(function (err, dir, cleanup) {
+    t.error(err)
+
+    var filepath = path.join(dir, 'sync.tar')
+    var mfeed = multifeed(hypercore, ram, { valueEncoding: 'json' })
+    var media = blob()
+    var syncfile
+    var nodeId
+    var nodeVersion
+    var node = { type: 'node', lat: 1, lon: 2 }
+
+    mfeed.writer('default', function (err, w) {
+      t.error(err)
+      w.append(node, function (err) {
+        t.error(err)
+        setup()
+      })
+    })
+
+    function setup () {
+      var filepath = path.join(dir, 'sync.tar')
+      syncfile = Syncfile(filepath, dir)
+      syncfile.ready(addMedia)
+    }
+
+    function addMedia (err) {
+      t.error(err, 'syncfile setup ok')
+      var tasks = []
+      var n = 0
+      for (var i = 0; i < 500; i++) {
+        tasks.push(function (next) {
+          var writeStream = media.createWriteStream(`hi-res-${n++}.jpg`, next)
+          fs.createReadStream(path.join(__dirname, 'hi-res.jpg')).pipe(writeStream)
+        })
+      }
+      parallel(tasks, function (err) {
+        if (err) throw err
+        console.log('done, syncing')
+        sync()
+      })
+    }
+
+    function sync (err) {
+      t.error(err, 'add media ok')
+      var d = syncfile.replicateData({live: false})
+      var r = mfeed.replicate({live: false})
+      replicate(r, d, function (err) {
+        t.error(err, 'replicate osm ok')
+        var d = syncfile.replicateMedia()
+        var r = blobReplicate(media)
+        replicate(d, r, function (err) {
+          t.error(err, 'replicate media ok')
+          syncfile.close(reopen)
+        })
+      })
+    }
+
+    function reopen (err) {
+      t.error(err, 'closed ok')
+      syncfile = new Syncfile(filepath, dir)
+      syncfile.ready(sync2)
+    }
+
+    function sync2 (err) {
+      t.error(err, 'reopen ok')
+
+      mfeed = multifeed(hypercore, ram, { valueEncoding: 'json' })
+      mfeed.ready(function () {
+        var d = syncfile.replicateData({live: false})
+        var r = mfeed.replicate({live: false})
+        replicate(r, d, function (err) {
+          // HACK(noffle): ignore for now; bug in multifeed
+          if (err && err.message === 'premature close') err = undefined
+
+          t.error(err, 'second replicate osm ok')
+          var d = syncfile.replicateMedia()
+          var r = blobReplicate(media)
+          replicate(d, r, function (err) {
+            t.error(err, 'second replicate media ok')
+            check()
+          })
+        })
+      })
+    }
+
+    function check (err) {
+      t.error(err, 'second syncfile close ok')
+
+      syncfile._mfeed.feeds()[0].get(0, function (err, res) {
+        t.error(err, 'get ok')
+        t.deepEquals(res, node)
+
+        media.exists('hi-res-0.jpg', function (err, exists) {
+          t.error(err, 'read media ok')
+          t.ok(exists, 'media exists')
           t.end()
         })
       })
